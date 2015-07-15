8eea7de7efe747a1ef595d51e5afeea10a50b45a
Irene <wangshuixian.scutech@gmail.com>
2015-07-15 13:54:51 +0800

    addstoraged normal

--- atdb/Supermanage/AddStoraged.py
+++ atdb/Supermanage/AddStoraged.py
@@ -1,15 +1,15 @@
-#coding=utf-8
+#-*-coding=utf-8
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
 import unittest, time, re
 
 class Untitled(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
+        self.driver.set_window_size(800, 480)
         self.base_url = "http://192.168.82.34/dbackup"
         self.verificationErrors = []
         self.accept_next_alert = True
@@ -19,29 +19,27 @@ class Untitled(unittest.TestCase):
         driver.get("http://192.168.82.34/dbackup/index.php")
         self.assertEqual(u"鼎甲迪备备份服务器", driver.title)
         driver.find_element_by_id("UserNameID").clear()
-        driver.find_element_by_id("UserNameID").send_keys("admin")
+        driver.find_element_by_id("UserNameID").send_keys("dingjia")
         driver.find_element_by_id("PWID").clear()
-        driver.find_element_by_id("PWID").send_keys("admin")
+        driver.find_element_by_id("PWID").send_keys("123456")
         driver.find_element_by_id("LoginButton").click()
-        time.sleep(2)
-        user = driver.find_element_by_xpath('//*[@id="navigate"]/ul[1]/li[3]/a')
-        ActionChains(driver).move_to_element(user).perform()
-        driver.find_element_by_xpath('//*[@id="navigate"]/ul[1]/li[3]/ul/li[1]/a').click()
-        time.sleep(2)
-        driver.find_element_by_id("account-add").click()
-#        driver.find_element_by_id("txt_username").clear()
-        driver.find_element_by_id("txt_username").send_keys("dingjia")
-#        driver.find_element_by_id("txt_password").clear()
-        driver.find_element_by_id("txt_password").send_keys("dingjia123")
-#        driver.find_element_by_id("txt_re_password").clear()
-        driver.find_element_by_id("txt_re_password").send_keys("dingjia123")
-#        driver.find_element_by_id("txt_email").clear()
-        driver.find_element_by_id("txt_email").send_keys("scutech@scutech.com")
-#        driver.find_element_by_id("txt_phone").clear()
-        driver.find_element_by_id("txt_phone").send_keys("1234567890")
-        driver.find_element_by_id("cb_role_admin").click()
-        driver.find_element_by_id("cb_role_monitor").click()
-        driver.find_element_by_id("btn_submit_user").click()
+        time.sleep(3)
+        current_handle = self.driver.current_window_handle
+        driver.switch_to_frame("headerflag")
+        driver.find_element_by_id("sysManagerLink").click()
+        driver.switch_to_window(current_handle)
+        driver.find_element_by_xpath('//*[@id="navigate"]/ul[1]/li[2]/a').click()
+        driver.find_element_by_xpath("/html/body/div[4]/div[3]/div[1]/div[1]/div[1]/div/a").click()
+        driver.find_element_by_id("txt_ftp_name").clear()
+        driver.find_element_by_id("txt_ftp_name").send_keys("192.168.82.34")
+        driver.find_element_by_id("txt_ftp_address").clear()
+        driver.find_element_by_id("txt_ftp_address").send_keys("192.168.82.34")
+        driver.find_element_by_id("txt_ftp_path").clear()
+        driver.find_element_by_id("txt_ftp_path").send_keys("Files")
+        driver.find_element_by_css_selector("input.w80.input-retention-days").clear()
+        driver.find_element_by_css_selector("input.w80.input-retention-days").send_keys("2")
+        driver.find_element_by_id("add_ftp_submit").click()
+        self.assertEqual(u"提交成功", self.close_alert_and_get_its_text())
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
