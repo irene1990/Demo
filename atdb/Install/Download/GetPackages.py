27d672693dfa4c60c74e9c9d888366aa8556b2ef
Irene <wangshuixian.scutech@gmail.com>
2015-07-27 18:45:12 +0800

    change

--- atdb/Install/Download/GetPackages.py
+++ atdb/Install/Download/GetPackages.py
@@ -12,55 +12,47 @@ class GetPackages():
 		self.ftp.retrbinary("RETR %s"%filename, f, 8192)
 
 	def getLastDEBServer(self):
-		filepath = LANG.lastest1 + LANG.year + '/' + LANG.version + LANG.lastest2
-		self.ftp.cwd(filepath)
-		listname = self.ftp.nlst(LANG.server_deb)
+		self.ftp.cwd(LANG.lastest_path)
+		listname = self.ftp.nlst(LANG.deb_server)
 		self.getDown(listname)
 
 	def getLastDEBAgent(self):
-		filepath = LANG.lastest1 + LANG.year + '/' + LANG.version + LANG.lastest2
-		self.ftp.cwd(filepath)
-		listname = self.ftp.nlst(LANG.agent_deb)
+		self.ftp.cwd(LANG.lastest_path)
+		listname = self.ftp.nlst(LANG.deb_agent)
 		self.getDown(listname)
 
 	def getLastRPMAgent(self):
-		filepath = LANG.lastest1 + LANG.year + '/' + LANG.version + LANG.lastest2
-		self.ftp.cwd(filepath)
-		listname = self.ftp.nlst(LANG.agent_rpm)
+		self.ftp.cwd(LANG.lastest_path)
+		listname = self.ftp.nlst(LANG.rpm64_agent)
 		self.getDown(listname)
 
 	def getLastAIXAgent(self):
-		filepath = LANG.lastest1 + LANG.year + '/' + LANG.version + LANG.lastest2
-		self.ftp.cwd(filepath)
-		listname = self.ftp.nlst(LANG.agent_aix)
+		self.ftp.cwd(LANG.lastest_path)
+		listname = self.ftp.nlst(LANG.aix_agent)
 		self.getDown(listname)
 
 	def getReleaseDEBServer(self):
-		filepath = LANG.release_server1 + LANG.version + LANG.release_server2
-		self.ftp.cwd(filepath)
-		listname = self.ftp.nlst(LANG.server_deb)
+		self.ftp.cwd(LANG.release_server)
+		listname = self.ftp.nlst(LANG.deb_server)
 		self.getDown(listname)
 
 	def getReleaseDEBAgent(self):
-		filepath = LANG.release_server1 + LANG.version + LANG.release_server3
-		self.ftp.cwd(filepath)
-		listname = self.ftp.nlst(LANG.agent_deb)
+		self.ftp.cwd(LANG.release_agent)
+		listname = self.ftp.nlst(LANG.deb_agent)
 		self.getDown(listname)
 
 	def getReleaseRPMAgent(self):
-		filepath = LANG.release_server1 + LANG.version + LANG.release_server3
-		self.ftp.cwd(filepath)
-		listname = self.ftp.nlst(LANG.agent_rpm)
+		self.ftp.cwd(LANG.release_agent)
+		listname = self.ftp.nlst(LANG.rpm64_agent)
 		self.getDown(listname)
 
 	def getReleaseAIXAgent(self):
-		filepath = LANG.release_server1 + LANG.version + LANG.release_server3
-		self.ftp.cwd(filepath)
-		listname = self.ftp.nlst(LANG.agent_aix)
+		self.ftp.cwd(LANG.release_agent)
+		listname = self.ftp.nlst(LANG.aix_agent)
 		self.getDown(listname)
 
 	def getLastDEBServer3(self):
-		filepath = LANG.lastest31 + 'deb' + LANG.lastest32 + LANG.deb3 + '/'
+		filepath = LANG.lastest3_path + 'deb' + LANG.dbackup3_mid + 'x86_64'
 		self.ftp.cwd(filepath)
 		dir1 = self.ftp.nlst('*')
 		self.ftp.cwd(dir1[-1][18:-10])
@@ -70,3 +62,78 @@ class GetPackages():
 		self.getDown(listname1)
 		self.getDown(listname2)
 		self.getDown(listname3)
+
+	def getLastRPMDBackup3(self):
+		filepath = LANG.lastest3_path + 'rpm' + LANG.dbackup3_mid + 'x86_64'
+		self.ftp.cwd(filepath)
+		dir1 = self.ftp.nlst('*')
+		self.ftp.cwd(dir1[-1][18:-17])
+		listname1 = self.ftp.nlst('*common*')
+		listname2 = self.ftp.nlst('*libstdc*')
+		listname3 = self.ftp.nlst('*agent-3.0*')
+		listname4 = self.ftp.nlst('*agent*oracle*')
+		listname5 = self.ftp.nlst('*agent*postgres*')
+		listname6 = self.ftp.nlst('*agent*file*')
+		self.getDown(listname1)
+		self.getDown(listname2)
+		self.getDown(listname3)
+		self.getDown(listname4)
+		self.getDown(listname5)
+		self.getDown(listname6)
+
+	def getLastPPCDBackup3(self):
+		filepath = LANG.lastest3_path + 'rpm' + LANG.dbackup3_mid + 'ppc64'
+		print filepath
+		self.ftp.cwd(filepath)
+		dir1 = self.ftp.nlst('*')
+		self.ftp.cwd(dir1[-1][18:-23])
+		listname1 = self.ftp.nlst('*common*')
+		listname2 = self.ftp.nlst('*libstdc*')
+		listname3 = self.ftp.nlst('*agent-3.0*')
+		listname4 = self.ftp.nlst('*agent*oracle*')
+		listname5 = self.ftp.nlst('*agent*file*')
+		self.getDown(listname1)
+		self.getDown(listname2)
+		self.getDown(listname3)
+		self.getDown(listname4)
+		self.getDown(listname5)
+
+	def getReleaseDEBServer3(self):
+		filepath = LANG.release_dbackup3 + 'x86_64'
+		self.ftp.cwd(filepath)
+		listname1 = self.ftp.nlst('*common*')
+		listname2 = self.ftp.nlst('*backupd*')
+		listname3 = self.ftp.nlst('*storaged*')
+		self.getDown(listname1)
+		self.getDown(listname2)
+		self.getDown(listname3)
+
+	def getReleaseRPMDBackup3(self):
+		filepath = LANG.release_dbackup3 + 'x86_64'
+		self.ftp.cwd(filepath)
+		listname1 = self.ftp.nlst('*common*')
+		listname2 = self.ftp.nlst('*libstdc*')
+		listname3 = self.ftp.nlst('*agent-3.0*')
+		listname4 = self.ftp.nlst('*agent*oracle*')
+		listname5 = self.ftp.nlst('*agent*postgres*')
+		listname6 = self.ftp.nlst('*agent*file*')
+		self.getDown(listname1)
+		self.getDown(listname2)
+		self.getDown(listname3)
+		self.getDown(listname4)
+		self.getDown(listname5)
+		self.getDown(listname6)
+
+	def getReleasePPCDBackup3(self):
+		filepath = LANG.release_dbackup3 + 'ppc64'
+		self.ftp.cwd(filepath)
+		listname1 = self.ftp.nlst('*common*')
+		listname2 = self.ftp.nlst('*libstdc*')
+		listname3 = self.ftp.nlst('*agent-3.0*')
+		listname4 = self.ftp.nlst('*agent*oracle*')
+		listname5 = self.ftp.nlst('*agent*file*')
+		self.getDown(listname1)
+		self.getDown(listname2)
+		self.getDown(listname3)
+		self.getDown(listname4)
+		self.getDown(listname5)
--- atdb/Install/Download/LANG.py
+++ atdb/Install/Download/LANG.py
@@ -1,27 +1,29 @@
 year = '2015'
 version = '5.1'
 # lastest's type
-deb3 = 'x86_64'
-rpm3 = 'x86_64'
-aix3 = 'ppc64'
 #list parameters
-server_deb = '*server*amd*deb'
-agent_deb = '*agent*all*deb'
-agent_rpm = '*agent*x86_64.rpm'
-agent_aix = '*agent*dev.aix5.1.ppc.rpm'
-# add version between release_server1 and release_server2
-release_server1 = '/product_release/scutech/dbackup/'
-release_server2 = '/dbackup_server'
-release_server3 = '/dbackup_agent'
-release_server4 = '/dbackup3'
-release_server5 = '/dbackup_standby'
-# add version between release31 and release32
-release31 = '/product_release/scutech/dbackup/'
-release32 = '/dbackup3/'
-# add type(deb3,rpm3,aix3) between lastest31 and lastest2
-lastest31 = '/ftp_product_installer/dbackup3/'
-lastest32 = '/develop/debug/'
-# add (year + '/' + version) between lastest31 and lastest2
-lastest1 = '/ftp_product_installer/wddps/'
-lastest2 = '/dev'
+deb_server = '*server*amd*deb'
+deb_agent = '*agent*all.deb'
+exe_agent = '*agent*exe'
+rpm64_agent = '*agent*x86_64.rpm'
+rpm32_agent = '*agent*i686.rpm'
+aix_agent = '*agent*dev.aix5.1.ppc.rpm'
+dbackup3_mid = '/develop/debug/'
+deb_dbackup3 = 'deb'
+exe_dbackup3 = 'mingw'
+rpm_dbackup3 = 'rpm'
+rpm64_dbackup3 = 'x86_64'
+rpm32_dbackup3 = 'i686'
+ppc64_dbackup3 = 'ppc64'
+##-----------------------------------------------------------------------
+release_path = '/product_release/scutech/dbackup/' + version + '/'
+release_server = release_path + 'dbackup_server/'
+release_agent = release_path + 'dbackup_agent/'
+release_dbackup3 = release_path + 'dbackup3/'
+release_standby_rpm = release_path + 'dbackup_standby/'
+##-----------------------------------------------------------------------
+lastest_path = '/ftp_product_installer/wddps/' + year + '/' + version + '/dev/'
+lastest3_path = '/ftp_product_installer/dbackup3/'
+##----------------------------------------------------------------------
+
 
